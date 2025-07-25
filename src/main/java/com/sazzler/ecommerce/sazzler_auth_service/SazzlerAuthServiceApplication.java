38fab4ac45e768cb979a22e36c8407a58a1960b6
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-07-25 23:41:37 +0530

    initial commit

---
+++ src/main/java/com/sazzler/ecommerce/sazzler_auth_service/SazzlerAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sazzler.ecommerce.sazzler_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SazzlerAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SazzlerAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sazzler/ecommerce/sazzler_auth_service/SazzlerAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sazzler.ecommerce.sazzler_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SazzlerAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
