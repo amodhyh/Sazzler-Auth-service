02fa2a49fc005f37a7c5df042cd0080a4efc1b89
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-07-27 23:20:23 +0530

    sec config start

--- src/main/java/com/sazzler/ecommerce/sazzler_auth_service/Config/SecurityConfig.java
+++ src/main/java/com/sazzler/ecommerce/sazzler_auth_service/Config/SecurityConfig.java
@@ -1,4 +1,23 @@
 package com.sazzler.ecommerce.sazzler_auth_service.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    //which url paths should be secured, and which should not
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(customizer->customizer.disable());
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
+        http.formLogin(form->form.loginPage("/login").permitAll());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
 }
--- src/main/java/com/sazzler/ecommerce/sazzler_auth_service/DTO/UserRegReq.java
+++ src/main/java/com/sazzler/ecommerce/sazzler_auth_service/DTO/UserRegReq.java
@@ -1,4 +1,16 @@
 package com.sazzler.ecommerce.sazzler_auth_service.DTO;
+import lombok.Data;
 
+import java.util.Date;
+
+@Data
 public class UserRegReq {
+String username;
+String password;
+String email;
+String firstName;
+String lastName;
+Date dob;
+Date createdAt;
+
 }
