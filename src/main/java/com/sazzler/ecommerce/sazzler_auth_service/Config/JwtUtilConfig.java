0fac1cf57d0edca6b076b7b4404561ab20beab90
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-09-20 20:15:26 +0530

    feat: Created Services unit tests and added input validation with new Exceptions for the login  -added some commentes in JwtUtilConfig  -Added 2 service tests LoginServiceTest and RegisterServiceTest in the Sazzler Auth Service

--- src/main/java/com/sazzler/ecommerce/sazzler_auth_service/Config/JwtUtilConfig.java
+++ src/main/java/com/sazzler/ecommerce/sazzler_auth_service/Config/JwtUtilConfig.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//as the JWTUtil is in the api-def which is  a java library agnostic of SpringBoot, this class is used to
+// make the JWTUtil bean and give the ability to inject into other spring components
 @Configuration
 public class JwtUtilConfig {
     @Value("${jwt.secret}" )
